494330b6a68dbf3c08c8fc1ed06c06dcac0658bd
hiepxuan <hiepxuan2006@gmail.com>
2023-04-14 04:17:13 +0700

    up

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { authorizationRequest } from "~/slices/authSlice"
 import { io } from "socket.io-client"
 export const socket = io(
-  "ws://api-chat-server-hiepxuan2006-dev.apps.sandbox-m3.1530.p1.openshiftapps.com",
+  "https://api-chat-server-hiepxuan2006-dev.apps.sandbox-m3.1530.p1.openshiftapps.com",
   {
     reconnection: true,
     reconnectionDelay: 1000,
