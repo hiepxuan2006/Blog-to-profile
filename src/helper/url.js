e18b0a197ecdfa9b146af41a42e071bfea05bd29
hiepxuan <hiepxuan2006@gmail.com>
2023-04-16 01:07:19 +0700

    up

--- src/helper/url.js
+++ src/helper/url.js
@@ -1,3 +1,5 @@
-export const URL =
-  "https://api-chat-server-hiepxuan2006-dev.apps.sandbox-m3.1530.p1.openshiftapps.com"
+// export const URL =
+//   "https://api-chat-server-hiepxuan2006-dev.apps.sandbox-m3.1530.p1.openshiftapps.com"
 // export const URL = "http://localhost:5005"
+export const URL =
+  "http://localhost:5005https://api-chat-server-hiepxuan2006-2-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com"
